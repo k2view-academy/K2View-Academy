911da609add0c0318b6f8ac770d644056cf5f99e
taliei <tali.einhorn@k2view.com>
2020-06-30 19:18:31 +0300

    Update demo project

---
+++ articles/demo_project/Implementation/LogicalUnits/CustomerLU/Java/src/com/k2view/cdbms/usercode/lu/CustomerLU/Globals.java
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////
+// LU Globals
+/////////////////////////////////////////////////////////////////////////
+
+package com.k2view.cdbms.usercode.lu.CustomerLU;
+
+import com.k2view.cdbms.usercode.common.SharedGlobals;
+import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
+
+public class Globals extends SharedGlobals {
+
+	
+
+
+}
